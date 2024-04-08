147c0b7080c352276031b1d7d008cd07a91af915
Winston Lam <winston.lam@outlook.com>
2024-04-08 16:43:45 +0200

    renamed variables

--- front-end/src/api.js
+++ front-end/src/api.js
@@ -3,14 +3,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_PRIVATE_KEY_ID,
+  apiKey: process.env.REACT_APP_FIREBASE_API,
   authDomain: `${process.env.REACT_APP_FIREBASE_PROJECT_ID}.firebaseapp.com`,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
 };
 
 const BASE_URL = process.env.REACT_APP_BACKEND_URL;
-console.log("BASE_URL", BASE_URL);
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
