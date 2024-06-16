a85903c2bb9787af5acc2a4ce91ed6e323199578
Winston Lam <winston.lam@outlook.com>
2024-06-16 11:12:59 +0200

    added themes, workin on styling

--- front-end/src/styles/themes.tsx
+++ front-end/src/styles/themes.tsx
@@ -1,21 +1,33 @@
 export const themes = {
   aqua: {
-    primary: "18, 72, 107",
-    second: "65, 145, 151",
-    third: "120, 214, 198",
-    fourth: "245, 252, 205",
+    primary: "96, 150, 180",
+    second: "173, 196, 206",
+    third: "238, 224, 201",
+    text: "241, 240, 232",
   },
   midnight: {
     primary: "7, 15, 43",
     second: "27, 26, 85",
     third: "83, 92, 145",
-    fourth: "146, 144, 195",
+    text: "146, 144, 195",
   },
   sunny: {
     primary: "254, 255, 210",
     second: "255, 238, 169",
     third: "255, 191, 120",
-    fourth: "255, 125, 41",
+    text: "255, 125, 41",
+  },
+  candy: {
+    primary: "249, 245, 246",
+    second: "248, 232, 238",
+    third: "253, 206, 223",
+    text: "242, 190, 209",
+  },
+  forest: {
+    primary: "37, 67, 54",
+    second: "107, 138, 122",
+    third: "183, 181, 151",
+    text: "218, 211, 190",
   },
 };
 
