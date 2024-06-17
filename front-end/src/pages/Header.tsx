b96d6598eb3b3601ad05e2def5130583a758a6dc
Winston Lam <winston.lam@outlook.com>
2024-06-17 14:01:26 +0200

    added logo, fixed styling

--- front-end/src/pages/Header.tsx
+++ front-end/src/pages/Header.tsx
@@ -2,19 +2,27 @@ import React, { FC, useContext } from "react";
 import { AdminContext } from "../providers/AdminContext";
 import "../styles/Header.css";
 import GearSvg from "../icons/settings-gear";
+import { useNavigate } from "react-router-dom";
+const logo = require("../icons/main-logo.png") as string;
+
+
 
 interface HeaderProps {
   setSideNav: (sideNav: boolean) => void;
 }
 
 const Header: FC<HeaderProps> = ({ setSideNav }) => {
+  const navigate = useNavigate();
   const { loggedIn } = useContext(AdminContext);
   return (
     <div>
       <div className="header">
         <div className="header-content">
-          <div className="header-item">
-            <h1>TimeItIn</h1>
+          <div className="header-logo" onClick={() => navigate("/")}>
+            <h2>TimeItIn</h2>
+            <img src={logo} alt="logo" className="logo-icon" />
+            <h2>Originals</h2>
+
           </div>
           {loggedIn ? <GearSvg
             className="header-gear-icon"
